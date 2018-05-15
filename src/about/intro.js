230dcf4914d2988d897b1bb6808814160c091374
wang <18463119857@163.com>
2018-05-15 22:19:28 +0800

    个人主页

--- src/about/intro.js
+++ src/about/intro.js
@@ -9,7 +9,7 @@ const HeadPor = () => (
 //个人简介
 const Info = () => (
     <div className="info-me">
-        <p className="info-title"><span>王蒙</span>|<span>web前端</span>|<span>23岁</span>|<span>Email: wangmengdesign@yeah.net</span></p>
+        <p className="info-title"><span>王蒙</span>|<span>web前端 & 设计</span>|<span>23岁</span>|<span>Email: wangmengdesign@yeah.net</span></p>
         <p className="info-link">Github</p>
         <p className="info-intro">爱前端，爱设计，希望成为全栈工程师。秉承少即多的设计理念。</p>
     </div>   
@@ -17,50 +17,53 @@ const Info = () => (
 
 //个人技能
 const Skills = () => (
-    <ul className="skills">
-        <li className="html">
-            <Motion defaultStyle={{width:0}}
-                style={{width: spring(200)}}
-            >
-                {value => <p className="html-anima" style={{width: value.width}}></p>}
-            </Motion>
-        </li>
-        <li className="javascript">
-            <Motion defaultStyle={{width:0}}
-                style={{width: spring(250)}}
-            >
-                {value => <p className="javascript-anima" style={{width: value.width}}></p>}
-            </Motion>
-        </li>
-        <li className="react">
-            <Motion defaultStyle={{width:0}}
-                style={{width: spring(150)}}
-            >
-                {value => <p className="react-anima" style={{width: value.width}}></p>}
-            </Motion>
-        </li>
-        <li className="query">
-            <Motion defaultStyle={{width:0}}
-                style={{width: spring(100)}}
-            >
-                {value => <p className="query-anima" style={{width: value.width}}></p>}
-            </Motion>
-        </li>
-        <li className="node">
-            <Motion defaultStyle={{width:0}}
-                style={{width: spring(100)}}
-            >
-                {value => <p className="node-anima" style={{width: value.width}}></p>}
-            </Motion>
-        </li>
-        <li className="photoshop">
-            <Motion defaultStyle={{width:0}}
-                style={{width: spring(200)}}
-            >
-                {value => <p className="photoshop-anima" style={{width: value.width}}></p>}
-            </Motion>
-        </li>
-    </ul>
+    <div className="skill-show">
+        <h3 className="caption">技能 & skills</h3>
+        <ul className="skills">
+            <li className="html">
+                <Motion defaultStyle={{width:0}}
+                    style={{width: spring(420)}}
+                >
+                    {value => <p className="html-anima" style={{width: value.width}}></p>}
+                </Motion>
+            </li>
+            <li className="javascript">
+                <Motion defaultStyle={{width:0}}
+                    style={{width: spring(390)}}
+                >
+                    {value => <p className="javascript-anima" style={{width: value.width}}></p>}
+                </Motion>
+            </li>
+            <li className="react">
+                <Motion defaultStyle={{width:0}}
+                    style={{width: spring(310)}}
+                >
+                    {value => <p className="react-anima" style={{width: value.width}}></p>}
+                </Motion>
+            </li>
+            <li className="query">
+                <Motion defaultStyle={{width:0}}
+                    style={{width: spring(325)}}
+                >
+                    {value => <p className="query-anima" style={{width: value.width}}></p>}
+                </Motion>
+            </li>
+            <li className="node">
+                <Motion defaultStyle={{width:0}}
+                    style={{width: spring(165)}}
+                >
+                    {value => <p className="node-anima" style={{width: value.width}}></p>}
+                </Motion>
+            </li>
+            <li className="photoshop">
+                <Motion defaultStyle={{width:0}}
+                    style={{width: spring(410)}}
+                >
+                    {value => <p className="photoshop-anima" style={{width: value.width}}></p>}
+                </Motion>
+            </li>
+        </ul>
+    </div>
 )
 
 export default () => (
